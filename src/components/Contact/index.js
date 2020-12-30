fd0b23d93023f52f8837f07524cf8619a968cee2
Michael Dreesen <michaeldreesen90@gmail.com>
2020-12-30 13:15:54 -0600

    added error handling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 
 // importing email.js
@@ -9,14 +9,43 @@ init("user_5PifaWCGrPQuL7ghKo7R5");
 
 export default function ContactUs() {
 
+    const [emailResponse, setEmailResponse] = useState({ 
+        ok: true,
+        visible: false,
+        message: ''
+    });
+    // const [emailSuccess, setEmailSuccess] = useState();
+
+
   function sendEmail(e) {
     e.preventDefault();
 
+    //type="email"
+
+    // user_5PifaWCGrPQuL7ghKo7R5
+
     emailjs.sendForm('service_u2qbro4', 'template_14mdvws', e.target, 'user_5PifaWCGrPQuL7ghKo7R5')
       .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
+          setEmailResponse({
+              ok: true,
+              visible: true,
+              message: result.text
+        })
+      }, (e) => {
+        setEmailResponse({
+            ok: false,
+            visible: true,
+            message: e.text
+      })
+      // calling a function whether its successful or not
+      }).finally(() => {
+        setTimeout(() => {
+            setEmailResponse({
+                ...emailResponse,visible:false
+
+            })
+        },
+        3000)
       });
   }
 
@@ -32,9 +61,9 @@ export default function ContactUs() {
                 </div>
                 <input type="text" name="user_name" />
                 <div>
-                    <label>Email</label>
+                    <label>Your Email</label>
                 </div>
-                <input type="email" name="user_email" />
+                <input name="user_email" />
                 <div>
                     <label>Message</label>
                 </div>
@@ -42,6 +71,7 @@ export default function ContactUs() {
                     <textarea name="message" />
                 </div>
                 <input className="submit-btn" type="submit" value="Send" />
+                <div>{emailResponse.visible ? emailResponse.message : ''}</div>
             </form>
             </div>
         </div>
